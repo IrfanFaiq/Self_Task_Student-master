8cccd7eedcc79c4e49d9bf23a100468eb1ad9366
laggerbomb <prolohzh@gmail.com>
2023-01-04 15:53:50 +0800

    Update AssignmentAdd.dart
    Remove unused import

--- lib/Presentation/Manage Assignmetn/AssignmentAdd.dart
+++ lib/Presentation/Manage Assignmetn/AssignmentAdd.dart
@@ -4,7 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:self_task_student/Bloc/Manage%20Assignment/assignment_bloc.dart';
 import 'package:self_task_student/Data/Database/SQLite/Model/AssignmentModel.dart';
-import 'AssignmentList.dart';
 
 class AssignmentAdd extends StatefulWidget{
   const AssignmentAdd({Key? key}) : super(key: key);
